f4ade451c92f8a973bcfa80897be11822503ba83
Dmitriy Lyubim <49170048+lyubimdmitriy03@users.noreply.github.com>
2024-10-11 15:53:01 +0300

    Add files via upload

---
+++ Gulp(4.0.2)_layout1.1/gulp/config/path.js
@@ -0,0 +1,43 @@
+//имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./assets`; //можно использовать название проекта
+const srcFolder = `./src`;
+
+export const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
+        files: `${buildFolder}/files/`,
+        libs: `${buildFolder}/libs/`
+    },
+    src: {
+        js: `${srcFolder}/js/*.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        scss: `${srcFolder}/scss/style.scss`,
+        rtl_scss: `${srcFolder}/rtl_scss/style-rtl.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+        libs: `${srcFolder}/libs/**/*.*`,
+        svgicons: `${srcFolder}/svgicons/*.svg`
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        rtl_scss: `${srcFolder}/rtl_scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,webp}`,
+        files: `${srcFolder}/files/**/*.*`,
+        libs: `${srcFolder}/libs/**/*.*`
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: `test_folder`
+}
---
+++ Gulp(4.0.2)_layout1.1/src/js/app.js
@@ -0,0 +1,9 @@
+import * as mainFunctions from "./modules/functions.js";
+import  "./modules/main.js";
+import  "./modules/form.js";
+mainFunctions.isWebp()
+
+
+
+
+
